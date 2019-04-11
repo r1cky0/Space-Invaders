f9ff72198a1a78787293b87175c2a5df3d8af791
Riccardo <riccardo.doveri01@unipv.it>
2019-04-11 10:58:17 +0200

    modifica coordinate

--- Progetto/src/game/environment/Coordinate.java
+++ Progetto/src/game/environment/Coordinate.java
@@ -2,31 +2,52 @@ package game.environment;
 
 public class Coordinate {
 
-    private double x, y;
+    private double x1, x2, y1, y2;
 
-    public Coordinate(){
-        x = 0;
-        y = 0;
+    public Coordinate(){ // possiamo usare la classe built in "point"??
+        x1 = 0;
+        x2 = 1;
+        y1 = 0;
+        y2 = 1;
     }
 
-    public Coordinate(double x, double y){
-        this.x = x;
-        this.y = y;
+    public Coordinate(double x1, double x2, double y1, double y2){
+        this.x1 = x1;
+        this.x2 = x2;
+        this.y1 = y1;
+        this.y2 = y2;
     }
 
-    public double getX() {
-        return x;
+
+    public double getX1() {
+        return x1;
     }
 
-    public double getY() {
-        return y;
+    public void setX1(double x1) {
+        this.x1 = x1;
     }
 
-    public void setX(double x) {
-        this.x = x;
+    public double getX2() {
+        return x2;
     }
 
-    public void setY(double y) {
-        this.y = y;
+    public void setX2(double x2) {
+        this.x2 = x2;
+    }
+
+    public double getY1() {
+        return y1;
+    }
+
+    public void setY1(double y1) {
+        this.y1 = y1;
+    }
+
+    public double getY2() {
+        return y2;
+    }
+
+    public void setY2(double y2) {
+        this.y2 = y2;
     }
 }
