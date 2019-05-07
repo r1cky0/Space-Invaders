977b0ab69c6649e8f2fd9e4da43409f6b77bbb69
luca <luca.colombo04@universitadipavia.it>
2019-05-07 15:49:38 +0200

    Facade

---
+++ MioSpaceInvaders/src/logic/sprite/unmovable/Brick.java
@@ -0,0 +1,32 @@
+package logic.sprite.unmovable;
+
+import logic.sprite.Coordinate;
+import logic.sprite.Sprite;
+
+public class Brick extends Sprite {
+
+    private int life;
+
+    public Brick(Coordinate coordinate, double size) {
+        super(coordinate, size);
+        life = 4;
+    }
+
+    public int getLife() {
+        return life;
+    }
+
+    public void setLife(int life) {
+        this.life = life;
+    }
+
+    public int decreaseLife() {
+        life -= 1;
+        return life;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+}
