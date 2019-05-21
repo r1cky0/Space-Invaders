3c339b5a780598531865f2d16e1eab24759caa2f
Riccardo <riccardo.doveri01@unipv.it>
2019-05-21 17:45:50 +0200

    aggiunto bottone newgame in gameoverstate

--- MioSpaceInvaders/src/gui/states/GameOverState.java
+++ MioSpaceInvaders/src/gui/states/GameOverState.java
@@ -4,36 +4,50 @@ import logic.environment.Menu;
 import org.newdawn.slick.*;
 import org.newdawn.slick.Graphics;
 import org.newdawn.slick.Image;
+import org.newdawn.slick.gui.AbstractComponent;
+import org.newdawn.slick.gui.ComponentListener;
+import org.newdawn.slick.gui.MouseOverArea;
 import org.newdawn.slick.state.BasicGameState;
 import org.newdawn.slick.state.StateBasedGame;
+import org.newdawn.slick.state.transition.FadeInTransition;
+import org.newdawn.slick.state.transition.FadeOutTransition;
 
 import java.awt.*;
 import java.awt.Font;
 
-public class GameOverState extends BasicGameState {
+public class GameOverState extends BasicGameState implements ComponentListener {
 
     private Image gameOver;
     private GameContainer container;
     private StateBasedGame stateBasedGame;
+
+    private MouseOverArea newGameButton;
+    private Image newGame;
 
     private Font UIFont1;
     private UnicodeFont uniFont;
 
     private Menu menu;
 
-    public GameOverState(Menu menu){
+    public GameOverState(Menu menu) {
         this.menu = menu;
     }
 
     public void init(GameContainer gameContainer, StateBasedGame stateBasedGame) throws SlickException {
-        this.container= gameContainer;
-        this.stateBasedGame= stateBasedGame;
+        this.container = gameContainer;
+        this.stateBasedGame = stateBasedGame;
+
+        newGame = new Image("res/images/newgame.png").getScaledCopy(gameContainer.getWidth() / 3,
+                gameContainer.getHeight() / 10);
+        newGameButton = new MouseOverArea(gameContainer, newGame, gameContainer.getWidth() / 3, 5 * gameContainer.getHeight() / 7,
+                gameContainer.getWidth() / 3, gameContainer.getHeight() / 10, this);
 
         gameOver = new Image("res/images/game_over.png");
     }
 
     public void render(GameContainer gameContainer, StateBasedGame stateBasedGame, Graphics graphics) throws SlickException {
-        gameOver.draw(container.getWidth()/7,container.getHeight()/6);
+        gameOver.draw(container.getWidth() / 7, container.getHeight() / 6);
+        newGameButton.render(gameContainer, graphics);
     }
 
     public void update(GameContainer gameContainer, StateBasedGame stateBasedGame, int i) throws SlickException {
@@ -46,4 +60,10 @@ public class GameOverState extends BasicGameState {
         return 3;
     }
 
+    @Override
+    public void componentActivated(AbstractComponent source) {
+        if (source == newGameButton) {
+            stateBasedGame.enterState(0, new FadeOutTransition(), new FadeInTransition()); // prima bisogna inizializzare lo start game di nuovo
+        }
+    }
 }
